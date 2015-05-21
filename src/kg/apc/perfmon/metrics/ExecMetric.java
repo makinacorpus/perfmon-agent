e759044990d857002d7c8262340300028ece35eb
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-21 21:59:25 +0200

    disable exec
    Signed-off-by: Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>

--- src/kg/apc/perfmon/metrics/ExecMetric.java
+++ src/kg/apc/perfmon/metrics/ExecMetric.java
@@ -25,32 +25,33 @@ class ExecMetric extends AbstractPerfMonMetric {
     }
 
     public void getValue(StringBuffer res) throws Exception {
-        log.debug("Executing custom script: " + MetricParams.join(null, command, " "));
+        log.debug("remote exec is disabled");
+        // log.debug("Executing custom script: " + MetricParams.join(null, command, " "));
+        // NO EXEC !
+        // try {
+        //     Process p = Runtime.getRuntime().exec(command);
 
-        try {
-            Process p = Runtime.getRuntime().exec(command);
+        //     BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        //     BufferedReader stdErr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        //     String e;
+        //     while ((e = stdErr.readLine()) != null) {
+        //         log.error("Error: " + e);
+        //     }
 
-            BufferedReader stdErr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-            String e;
-            while ((e = stdErr.readLine()) != null) {
-                log.error("Error: " + e);
-            }
-
-            // read the output from the command
-            String s, lastStr = "";
-            while ((s = stdInput.readLine()) != null) {
-                log.debug("Read proc out line: " + s);
-                lastStr = s;
-            }
-            res.append(lastStr);
-            
-            stdErr.close();
-            stdInput.close();
-            p.destroy();
-        } catch (IOException e) {
-            log.error("Problems executing: " + command[0], e);
-        }
+        //     // read the output from the command
+        //     String s, lastStr = "";
+        //     while ((s = stdInput.readLine()) != null) {
+        //         log.debug("Read proc out line: " + s);
+        //         lastStr = s;
+        //     }
+        //     res.append(lastStr);
+        //     
+        //     stdErr.close();
+        //     stdInput.close();
+        //     p.destroy();
+        // } catch (IOException e) {
+        //     log.error("Problems executing: " + command[0], e);
+        // }
     }
 }
